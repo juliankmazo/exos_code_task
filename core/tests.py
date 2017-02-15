26655b035aaf819767d2ac467d7571e2e2f8e4e5
juliankmazo <julian.kmazo@gmail.com>
2017-02-15 14:29:15 -0500

    Implement user list download link and view
    * Fix Fizz Buzz sintax to Bizz Fuzz
    * Add test for `BuzzFizz`

--- core/tests.py
+++ core/tests.py
@@ -47,18 +47,26 @@ class UserAppTest(TestCase):
 
         self.assertIn('Blocked', str(response.content))
 
-    def test_fizz(self):
+    def test_bizz(self):
         user_1 = User(username='user_1', password='test', random_number=3)
         user_1.save()
 
         response = self.client.get('/users/1/')
 
-        self.assertIn('Fizz', str(response.content))
+        self.assertIn('Bizz', str(response.content))
 
-    def test_buzz(self):
+    def test_fuzz(self):
         user_1 = User(username='user_1', password='test', random_number=5)
         user_1.save()
 
         response = self.client.get('/users/1/')
 
-        self.assertIn('Buzz', str(response.content))
+        self.assertIn('Fuzz', str(response.content))
+
+    def test_bizz_fuzz(self):
+        user_1 = User(username='user_1', password='test', random_number=15)
+        user_1.save()
+
+        response = self.client.get('/users/1/')
+
+        self.assertIn('BizzFuzz', str(response.content))
